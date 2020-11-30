cd6d4b2c1ead18d8737d652065bd9f43593b3bfe
Ramyarnayak <ramya.nayak@ymedialabs.com>
2020-11-30 12:17:59 +0530

    Render MovieList

--- react-redux-app/src/Components/HomePage.js
+++ react-redux-app/src/Components/HomePage.js
@@ -1,20 +1,12 @@
 import React from 'react';
-import {AddPost} from './AddPost';
-import {PostList} from './PostList';
 import MovieList from './MovieList';
-import MovieDetails from './MovieDetails';
-import AddReview from './AddReview';
+
 
 
 export const HomePage = () => {
   return (
     <>
     <MovieList/>
-    
-      {/* <AddPost />
-      <PostList /> */}
-       {/* <AddReview/>  */}
-
     </>
   )
 }
