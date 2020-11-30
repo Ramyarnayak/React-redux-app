e60474279020e1c109d74bf328bff94305c00cb9
Ramyarnayak <ramya.nayak@ymedialabs.com>
2020-11-30 11:14:01 +0530

    added more actions

--- react-redux-app/src/Components/MovieDetails.js
+++ react-redux-app/src/Components/MovieDetails.js
@@ -1,84 +1,80 @@
 import React,{ useState} from 'react'
-// import { connect } from 'react-redux'
-
 import { useSelector } from "react-redux";
 import AddReview from './AddReview';
 import '../styles/MovieDetails.css'
 
 
-
 const MovieDetails =(props) =>{
-    const[displayAddModal,setDisplayAddModal]=useState(false);
-
-    const [click, setClick] = useState(true);
-
-    const handleClick =()=>{
-        setClick(true)
-    }
-  
+    
     const selectedMovie = useSelector((state) => state.selectedMovieReducer); 
-    const review=()=>{
-        return(
-            <AddReview/>
-        )
-    }
  
     if(!selectedMovie){
         return(
+
         <div className="details-container" >
-
-            <h2> Movie</h2>
+            <h2 className="english-movie"> Latest English Movies</h2>
             <div className="properties">
-                <p>Select movie</p>
-            </div>
-
+                <div className="english-movie-desc">
+                    Hollywood is growing with each year and the number of 
+                    films releasing every has to has grown exponentially. Here 
+                    in this section, we give a list of latest Hollywood flicks which have released giving you
+                     an inside to their reviews. Not only that but this section even provides you with details
+                      like cast information, box-office collections, related videos and pictures of all the latest 
+                      Hollywood films. This latest Hollywood films listing is a one-stop 
+                    solution for all the latest updates about new English movies running in theatres.
+                </div>
+                <br></br>
+             <div className="select">Select a movie to know more about it</div>
+         </div>
         </div>
         )
     }
+
     else
     {
-        
         return(
+
             <div className="details-container" style={{width:'70%'}}>
-            
             <div className="properties">
-             
-         <h2 className="poster">Title:{selectedMovie.trailor}</h2>
-         <div className="container">
-         <span className="title-details">{selectedMovie.title}</span>
-         
-                <span className="movie-desc-details"> {selectedMovie.desc}</span>
-                <span className="movie-ratings-details">Ratings: {selectedMovie.rating}</span>
-                <span className="movie-type-details"> {selectedMovie.type}</span>
-                <span className="movie-date-details"> {selectedMovie.releaseDate}</span>
-                <span className="movie-reviewoneline-details">{selectedMovie.reviewoneline}</span>
+                 <h2 className="poster">{selectedMovie.poster}</h2>
+                 <div className="container">
+                    <span className="title-details">{selectedMovie.title}</span>
+                    <span className="movie-desc-details"> {selectedMovie.desc}</span>
+                    <span className="movie-ratings-details">Ratings: {selectedMovie.rating}</span>
+                    <span className="movie-type-details"> {selectedMovie.type}</span>
+                    <span className="movie-date-details"> {selectedMovie.releaseDate}</span>
+                    <span className="movie-reviewoneline-details">{selectedMovie.reviewoneline}</span>
                 </div>
-       <div className="container2">
-        <span className="synopsis"><span className="headings">Synopsis:</span>{selectedMovie.synopsis}</span>
         
-        <span className="critics-ratings"><span className="headings">Critic's Ratings:</span>{selectedMovie.rating}</span>
-        <span className="story"><span className="headings">{selectedMovie.title} story:</span>{selectedMovie.story}</span>
-        <span className="fullreview"><span className="headings">{selectedMovie.title} Movie Review:</span>{selectedMovie.reviewdetail}</span>
+         <div className="container2">
+
+         <span className="synopsis">
+             <span className="headings">Synopsis:</span>
+             <div className="content">{selectedMovie.synopsis}</div>
+         </span>
+        
+        
+        <span className="story">
+            <span className="headings">{selectedMovie.title} story:</span>
+            <div className="content">{selectedMovie.story}</div>
+        </span>
+        
+         <span className="fullreview">
+             <span className="headings">{selectedMovie.title} Movie Review:</span>
+            <div className="content">{selectedMovie.reviewdetail}</div>
+        </span>
 
         <span className="review">:{selectedMovie.review}</span>
-        </div>
-{/*         
-        <button onClick={()=>console.log("hi")}>Watch trailor</button> */}
+     </div>
 
-         <button onClick={click ? <AddReview/> : <MovieDetails/>}>Write review</button> 
-        
        
-            </div>
+    </div>
           
-        </div>
+     </div>
         )
     }
     
 }
-// const mapStateProps =(state) =>{
-//     return{
-//         selectedMovie : state.selectedMovie
-//     }
-// }
+
 
 export default MovieDetails;
--- react-redux-app/src/store/actions/postAction.js
+++ react-redux-app/src/store/actions/postAction.js
@@ -1,16 +1,26 @@
-export const addPost = (post) => {
-  return { type: "ADD_POST", post }
-}
 
-export const deletePost = (id) => {
-  return { type: "DELETE_POST", id }
-}
 export const selectMovie =(movie) =>{
   return{
       type:'MOVIE_SELECTED',
       payload: movie,
   };
+}; 
+ export const handleDelete =(id)=> {
+     return{   
+  type:'DELETE_REVIEW',
+  payload: id
+     };
 };
+export const handleAdd =(newReview)=>{
+  return{      
+  type:'ADD_REVIEW',
+  payload: {
+    label: newReview,
+    id: Math.ceil(Math.random() * 100),
+  }
+  };
+};
+
 
 
 
