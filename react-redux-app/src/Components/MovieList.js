72675bad9c4bc97738fcb582f8729ce6460e0ed1
Ramyarnayak <ramya.nayak@ymedialabs.com>
2020-11-30 11:09:44 +0530

    final commit

--- react-redux-app/src/Components/MovieList.js
+++ react-redux-app/src/Components/MovieList.js
@@ -1,52 +1,51 @@
 import React from 'react';
-import {selectMovie , trailor} from '../store/actions/postAction';
+import { selectMovie } from '../store/actions/postAction';
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
-import AddReview from './AddReview';
 import MovieDetails from './MovieDetails'
 import '../styles/MovieList.css'
 
 
 const MovieList = (props) =>{
+    
     const dispatch = useDispatch();
   
     const movies = useSelector((state) => state.movieReducer);
-    // const trailor = useSelector((state) => state.movieTrailor);
 
     const listItems = movies.map((movie) => {
+
         return(
             <div className="main">
-            <div className="movie-card" key={movie.title}>
-                <span>{movie.trailor}</span>
-                <div className="movie-summary">
-                <span className="title">{movie.title}</span>
-                <span className="movie-desc">{movie.desc}</span>
-                <span className="movie-ratings">Ratings: {movie.rating}</span>
-                <span className="movie-type"> {movie.type}</span>
-                <span className="movie-date"> {movie.releaseDate}</span>
+                <div className="movie-card" key={movie.title}>
+                    <span>{movie.poster}</span>
+                    
+                    <div className="movie-summary">
+                        <span className="title">{movie.title}</span>
+                        <span className="movie-desc">{movie.desc}</span>
+                        <span className="movie-ratings">Ratings: {movie.rating}</span>
+                        <span className="movie-type"> {movie.type}</span>
+                        <span className="movie-date"> {movie.releaseDate}</span>
+                        <span>{}</span>
                 
-                
-        <span>{}</span>
-                <button className="btn-details" onClick={()=> dispatch(selectMovie(movie))}>Details</button>
-                </div>
-            </div>  
+                        <button className="btn-details" onClick={()=> dispatch(selectMovie(movie))}><span className="btn-span">Know more</span></button>
+                    </div>
+                </div>  
             </div>      
             )
     })
+
+
     return(
         <>
+         <div class="header"><h1>Welcome to <em>MOVIE_WORLD!!!</em></h1></div>
+      
         <div className="list-conatiner">
-         
-
             <ul className="ul-list">
-                
                 {listItems}
-
             </ul>
-           
-
         </div>
-         <MovieDetails/>
+        
+        <MovieDetails/>
          </>
     )
 }
